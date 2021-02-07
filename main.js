66a83be272aeb486ab922260a159e908f0ced03a
nekoama <rtanmail2@gmail.com>
2021-02-07 13:40:54 -0800

    2/07

--- main.js
+++ main.js
@@ -1,11 +1,2 @@
 $(".navbar").load("./includes/navbar.html");
 $(".footer").load("./includes/footer.html");
-
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
