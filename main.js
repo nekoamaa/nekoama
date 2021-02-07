ae1b5795ab9e49a5957501d0f40c4264dda3889d
nekoama <rtanmail2@gmail.com>
2021-02-07 15:28:40 -0800

    transparent navbar

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 $(".navbar").load("./includes/navbar.html");
 $(".footer").load("./includes/footer.html");
+
+$(function() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 10) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
